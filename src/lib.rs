a3e4f61fcff56a488e4bd91fd5daba1cf3388003
Tobias Kolditz <tbs.kldtz@gmail.com>
2021-04-03 08:24:06 +0200

    Remove duplication, apply petty optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -1,106 +1,53 @@
 //! CISTEM Stemmer for German. Rust translation of [CISTEM](https://github.com/LeonieWeissweiler/CISTEM).
-use std::borrow::Cow;
 
 /// This method takes the word to be stemmed and a boolean specifiying if case-insensitive stemming should be used and returns the stemmed word.
-/// 
+///
 /// Case sensitivity improves performance only if words in the text may be incorrectly upper case.
 /// For all-lowercase and correctly cased text, best performance is achieved by
 /// using the case-sensitive version.
-/// 
+///
 /// ```
 /// use cistemrs::stem;
-/// 
+///
 /// let s = stem("schönes", false);
 /// assert_eq!(s, "schon");
 /// ```
-pub fn stem<'a>(word: &'a str, case_insensitive: bool) -> Cow<'a, str> {
+pub fn stem(word: &str, case_insensitive: bool) -> String {
     if word.len() == 0 {
-        return word.into();
+        return "".into();
     }
     let upper = word.chars().next().unwrap().is_uppercase();
-    // replace single chars
-    let mut stem = String::with_capacity(word.len());
+    
+    // normalize string
+    let mut normalized = String::with_capacity(word.len());
     // keep track of number of chars
     let mut num_chars = 0;
     for ch in word.chars() {
         num_chars += 1;
         for c in ch.to_lowercase() {
             match c {
-                'ü' => stem.push('u'),
-                'ö' => stem.push('o'),
-                'ä' => stem.push('a'),
+                'ü' => normalized.push('u'),
+                'ö' => normalized.push('o'),
+                'ä' => normalized.push('a'),
                 'ß' => {
-                    stem.push_str("ss");
+                    normalized.push_str("ss");
                     num_chars += 1;
                 }
-                _ => stem.push(c),
+                _ => normalized.push(c),
             };
         }
     }
 
-    // cut off prefix
-    if stem.starts_with("ge") && num_chars >= 6 {
-        stem = stem[2..].to_string();
+    // compute start offset
+    let start = if normalized.starts_with("ge") && num_chars >= 6 {
         num_chars -= 2;
-    }
+        2
+    } else {
+        0
+    };
 
-    let num_bytes = stem.len();
-    //replace multiple chars
-    stem = stem
-        .replace("sch", "$")
-        .replace("ei", "%")
-        .replace("ie", "&");
-    // only 8-bit chars are replaced, so this is valid
-    num_chars -= num_bytes - stem.len();
-    stem = replxx(stem);
-
-    // trim suffixes
-    while num_chars > 3 {
-        if num_chars > 5 && (stem.ends_with("em") || stem.ends_with("er") || stem.ends_with("nd")) {
-            stem.truncate(stem.len() - 2);
-            num_chars -= 2;
-        } else if ((!upper || case_insensitive) && stem.ends_with("t"))
-            || stem.ends_with(&['e', 's', 'n'][..])
-        {
-            stem.truncate(stem.len() - 1);
-            num_chars -= 1;
-        } else {
-            break;
-        }
-    }
-
-    stem = undo_replacements(stem, word.len());
-    stem.into()
-}
-
-fn replxx(stem: String) -> String {
-    let mut result = String::with_capacity(stem.len());
-    for c in stem.chars() {
-        if result.ends_with(c) {
-            result.push('*');
-        } else {
-            result.push(c);
-        }
-    }
-    result
-}
-
-fn undo_replacements(stem: String, word_len: usize) -> String {
-    let mut result = String::with_capacity(word_len);
-    let mut last_repl = "*";
-    let mut b = [0; 4];
-    for c in stem.chars() {
-        let repl = match c {
-            '*' => last_repl,
-            '%' => "ei",
-            '&' => "ie",
-            '$' => "sch",
-            _ => c.encode_utf8(&mut b),
-        };
-        result.push_str(repl);
-        last_repl = repl;
-    }
-    result
+    let end = start + compute_end_offset(&normalized[start..], num_chars, upper, case_insensitive);
+    String::from(&normalized[start..end]).into()
 }
 
 /// This method works very similarly to stem. The only difference is that in
@@ -108,10 +55,10 @@ fn undo_replacements(stem: String, word_len: usize) -> String {
 /// the end. To be able to return the stem unchanged so the stem and the rest
 /// can be concatenated to form the original word, all subsitutions that altered
 /// the stem in any other way than by removing letters at the end were left out.
-/// 
+///
 /// ```
 /// use cistemrs::segment;
-/// 
+///
 /// let (l, r) = segment("schönes", false);
 /// assert_eq!(l, "schön");
 /// assert_eq!(r, "es");
@@ -122,36 +69,52 @@ pub fn segment(word: &str, case_insensitive: bool) -> (String, String) {
     }
     let upper = word.chars().next().unwrap().is_uppercase();
     let word = word.to_lowercase();
+    let end = compute_end_offset(&word, word.chars().count(), upper, case_insensitive);
+    return (word[..end].into(), word[end..].into());
+}
 
-    //replace multiple chars
-    let mut stem = word
+fn compute_end_offset(word: &str, mut num_chars: usize, upper: bool, case_insensitive: bool) -> usize {
+    // temporary replacements
+    let mut repl = word
         .replace("sch", "$")
         .replace("ei", "%")
         .replace("ie", "&");
-    stem = replxx(stem);
+    repl = replxx(repl);
+    // only 8-bit chars are replaced, so this is valid
+    num_chars -= word.len() - repl.len();
 
-    let mut num_chars = stem.chars().count();
-    
-    // trim suffixes
+    //let mut num_chars = repl.chars().count();
+    let mut end = word.len();
+    // compute end offset
     while num_chars > 3 {
-        if num_chars > 5 && (stem.ends_with("em") || stem.ends_with("er") || stem.ends_with("nd")) {
-            stem.truncate(stem.len() - 2);
+        if num_chars > 5 && (repl.ends_with("em") || repl.ends_with("er") || repl.ends_with("nd")) {
+            repl.truncate(repl.len() - 2);
+            end -= 2;
             num_chars -= 2;
-        } else if ((!upper || case_insensitive) && stem.ends_with("t"))
-            || stem.ends_with(&['e', 's', 'n'][..])
+        } else if ((!upper || case_insensitive) && repl.ends_with("t"))
+            || repl.ends_with(&['e', 's', 'n'][..])
         {
-            stem.truncate(stem.len() - 1);
+            repl.truncate(repl.len() - 1);
+            end -= 1;
             num_chars -= 1;
         } else {
             break;
         }
     }
-
-    stem = undo_replacements(stem, word.len());
-
-    return (word[..stem.len()].into(), word[stem.len()..].into());
+    end
 }
 
+fn replxx(stem: String) -> String { 
+    let mut result = String::with_capacity(stem.len());
+    for c in stem.chars() {
+        if result.ends_with(c) {
+            result.push('*');
+        } else {
+            result.push(c);
+        }
+    }
+    result
+}
 
 
 #[cfg(test)]
@@ -176,7 +139,10 @@ mod tests {
             let stem_ci = stem(word, true);
             let (left, right) = segment(word, false);
             let (left_ic, right_ic) = segment(word, true);
-            let actual_line = format!("{}\t{}\t{}\t{}\t{}\t{}\t{}", word, stem_cs, stem_ci, left, right, left_ic, right_ic);
+            let actual_line = format!(
+                "{}\t{}\t{}\t{}\t{}\t{}\t{}",
+                word, stem_cs, stem_ci, left, right, left_ic, right_ic
+            );
             assert_eq!(actual_line, line);
         }
     }
